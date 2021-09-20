d090508594f24100932b6a9aca2e78098134bbac
Gerald Tan <tanjunweigerald1997@gmail.com>
2021-09-20 13:50:40 +0800

    Changed LPRNet code for API usage

--- python_backend/triton_client/model_client/lpr_model_class.py
+++ python_backend/triton_client/model_client/lpr_model_class.py
@@ -1,10 +1,10 @@
 import os
 import requests
 from base_model_class import BaseModelClass
-from lpd_client import lpd_predict
+from lpr_client import lpr_predict
 from requests.exceptions import ConnectionError
 
-class LpdModelClass(BaseModelClass):
+class LprModelClass(BaseModelClass):
 
     def __init__(self, client_info):
         '''
@@ -15,9 +15,10 @@ class LpdModelClass(BaseModelClass):
         BaseModelClass.__init__(self, client_info)
         self._post_processing_config = "/app/triton_client/model_client/tao_triton/python/clustering_specs/clustering_config_lpdnet.prototxt"
         self._url = "35.240.147.255:6000"
-        self._model_name = "lpdnet_usa"
-        self._mode = "DetectNet_v2"
+        self._model_name = "lprnet_usa"
+        self._mode = "Lprnet"
         self._class_list = "license_plate"
+        self.mapping_output_file = "../postprocessing_config/us_lp_characters.txt"
     
     def status(self):
         '''
@@ -31,7 +32,6 @@ class LpdModelClass(BaseModelClass):
         else:
             return {'HTTPStatus' : 200, 'status':'Active'}
                 
-    
     def predict(self, file_path):
         '''
         Returns the output of the model after inference.
@@ -44,17 +44,19 @@ class LpdModelClass(BaseModelClass):
 
     def _predict(self, file_path):
         number_files = len([name for name in os.listdir(file_path) if os.path.isfile(file_path+name)])
+        print(number_files)
         if number_files < 256:
             self._batch_size = 8
         else:
             self._batch_size = 32
-        return lpd_predict(model_name = self._model_name, mode = self._mode, class_list = self._class_list, \
-                            output_path = "./", postprocessing_config = self._post_processing_config, \
+        return lpr_predict(model_name = self._model_name, mode = self._mode, class_list = self._class_list, \
+                            output_path = "./output/lprnet_usa/", postprocessing_config = self._post_processing_config, \
                             url = self._url, image_filename = file_path, verbose = False, streaming = False, async_set = False, \
-                            protocol = 'HTTP', model_version = "", batch_size = self._batch_size)
+                            protocol = 'HTTP', model_version = "", batch_size = 1, \
+                                mapping_output_file = "../postprocessing_config/us_lp_characters.txt")
 
-#To handle output_path
+#To handle output_pathi
 
-test_model = LpdModelClass("hellosss");
-print(test_model.predict("../input/lpd"))
-# print(test_model.status())
+test_model = LprModelClass("hellosss");
+# print(test_model.predict("../input/"))
+print(test_model.predict("../input/lpr/"))
