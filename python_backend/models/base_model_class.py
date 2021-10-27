09b9e1bbbd52a3d5fcccf48cfd210d7b216e05d3
Rance <limjyrance@gmail.com>
2021-10-27 21:01:32 +0800

    Implement model specific API checks

--- python_backend/models/base_model_class.py
+++ python_backend/models/base_model_class.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import json
+import os
+import requests
 
 class BaseModelClass:
 
@@ -8,20 +11,36 @@ class BaseModelClass:
     called by the backend.
     '''
 
-    def __init__(self, client_info):
+    def __init__(self, client_info, url, model_name):
         '''
         Instantiate the classes with the information of the 
         querying party -- corresponding to a specific triton
         model.
         '''
         self._client_info = client_info
+        self._url = url
+        self._model_name = model_name
     
-    @abstractmethod
     def status(self):
         '''
         Returns the status of the model
         '''
-        pass
+        triton_server_url = f'http://{self._url}/v2/repository/index'
+        try:
+            response = requests.post(triton_server_url)
+            body = json.loads(response.text)
+        except ConnectionError:
+            return {'status': 'Inactive'}
+        
+        model = list(filter(lambda model: model['name'] == self._model_name, body))
+        if model:
+            state = model[0]['state']
+            if state == 'READY':
+                return {'status': 'Active'}
+            else:
+                return {'status': 'Inactive'}
+        else:
+            return {'status': 'Model not found'}   
 
     
     @abstractmethod
